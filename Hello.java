7320945c5471cef44b1f3240c0b9f20ad2a09990
ldfkalinski <ldfkalinski@minha.fag.edu.br>
2026-03-04 21:34:50 -0300

    Italiaaaaaaaaaaaaaaaano-Maaaaario

--- Hello.java
+++ Hello.java
@@ -1,8 +1,12 @@
 public class Hello{
     public static void main(String[] args) {
         System.out.println("hello world");
+        italiaano();
     }
-    public static void alemao{
+    public static void alemon(){
         System.out.println("Hallo Welt!");
+    }
+    public static void italiaano() {
+        System.out.println("Ciao Mondo!");
     }
 }
