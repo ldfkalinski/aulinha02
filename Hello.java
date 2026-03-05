935b61cf47678880929649e5e5425f89cf154ce3
ldfkalinski <ldfkalinski@minha.fag.edu.br>
2026-03-04 21:26:42 -0300

    ALEMAO

--- Hello.java
+++ Hello.java
@@ -2,4 +2,7 @@ public class Hello{
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+    public static void alemao{
+        System.out.println("Hallo Welt!");
+    }
 }
